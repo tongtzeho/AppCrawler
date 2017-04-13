39f1c884280dfac02a0b3d9aa5c328f0b0d9ddee
tongtzeho <tangzihao@pku.edu.cn>
2017-04-13 17:28:45 +0800

    Update _database.py

--- _database.py
+++ _database.py
@@ -67,11 +67,11 @@ def parse_number(market, line):
 	if len(numstr):
 		for cnword in cnword_multi_2.keys():
 			if line.endswith(cnword):
-				return str(int(float(numstr[-1])*cnword_multi_2[cnword]))
+				return str(round(float(numstr[-1])*cnword_multi_2[cnword]))
 		for cnword in cnword_multi_1.keys():
 			if line.endswith(cnword):
-				return str(int(float(numstr[-1])*cnword_multi_1[cnword]))
-		return str(int(float(numstr[-1])))
+				return str(round(float(numstr[-1])*cnword_multi_1[cnword]))
+		return str(round(float(numstr[-1])))
 	return ""
 
 def parse_rating(market, line):
