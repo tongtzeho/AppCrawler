ebcae91a800413e6c23d890901224b1f02f3f9b9
tongtzeho <tangzihao@pku.edu.cn>
2017-06-13 11:12:22 +0800

    Update _crawler.py

--- _crawler.py
+++ _crawler.py
@@ -249,7 +249,7 @@ def main_loop(threadidstr, market, root, thread_num, rate_per_iteration, lock_po
 		random.shuffle(url_list)
 		for short_url in url_list:
 			try:
-				if update >= thread_num*100: #10:
+				if update >= thread_num*500: #10:
 					update = 0
 					lock_set.acquire()
 					hold_lock_set = True
